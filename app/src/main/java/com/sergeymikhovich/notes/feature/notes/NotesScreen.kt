695691d79d440a208a8d37dabc91582200562046
Sergey Mikhovich <srgmikh1998@gmail.com>
2024-09-09 00:04:22 +0300

    redesign notes screen

--- app/src/main/java/com/sergeymikhovich/notes/feature/notes/NotesScreen.kt
+++ app/src/main/java/com/sergeymikhovich/notes/feature/notes/NotesScreen.kt
@@ -2,19 +2,24 @@ package com.sergeymikhovich.notes.feature.notes
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -30,8 +35,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -40,8 +47,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import com.sergeymikhovich.notes.R
-import com.sergeymikhovich.notes.app.ui.theme.NotesTheme
-import com.sergeymikhovich.notes.app.ui.theme.Purple40
 import com.sergeymikhovich.notes.core.common.navigation.composableTo
 import com.sergeymikhovich.notes.core.model.Note
 import com.sergeymikhovich.notes.feature.notes.navigation.NotesDirection
@@ -49,95 +54,156 @@ import com.sergeymikhovich.notes.feature.notes.navigation.NotesDirection
 fun NavGraphBuilder.composableToNotes() = composableTo(NotesDirection) { NotesScreen() }
 
 @Composable
-fun NotesScreen(viewModel: NotesViewModel = hiltViewModel()) {
-    NotesTheme {
-        Scaffold(
-            floatingActionButton = {
+fun NotesScreen(
+    viewModel: NotesViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    NotesContent(
+        state = state,
+        onCreateNoteClick = viewModel::toCreateNote,
+        onAccountCenterClick = viewModel::toAccountCenter,
+        onOpenNoteClick = viewModel::toNote,
+        onDeleteNoteClick = viewModel::onDeleteNoteClick
+    )
+}
+
+@Composable
+private fun NotesContent(
+    state: NotesState,
+    onCreateNoteClick: () -> Unit,
+    onAccountCenterClick: () -> Unit,
+    onOpenNoteClick: (String) -> Unit,
+    onDeleteNoteClick: (String) -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            if (!state.isEmpty) {
                 FloatingActionButton(
-                    onClick = viewModel::toCreateNote,
+                    onClick = onCreateNoteClick,
                     modifier = Modifier.padding(16.dp),
-                    backgroundColor = Purple40,
+                    backgroundColor = Color(0xFFD9614C),
                     shape = RoundedCornerShape(16.dp)
                 ) {
-                    Icon(Icons.Filled.Add, "Add", tint = Color.White)
-                }
-            }
-        ) { paddingValues ->
-            val state by viewModel.state.collectAsStateWithLifecycle()
-
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                TopAppBar(
-                    title = { Text(text = stringResource(id = R.string.app_name)) },
-                    actions = {
-                        IconButton(onClick = viewModel::toAccountCenter) {
-                            Icon(Icons.Filled.Person, "Account center")
-                        }
-                    }
-                )
-                
-                Spacer(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp))
-
-                if (state.isEmpty) {
-                    EmptyNotes(onButtonClick = viewModel::toCreateNote)
-                } else {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Notes(
-                            notes = state.notes,
-                            onNoteClick = viewModel::toNote,
-                            onNoteLongClick = viewModel::onDeleteNoteClick
-                        )
-                    }
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add",
+                        tint = Color.White
+                    )
                 }
             }
         }
+    ) { paddingValues ->
 
+        Column(
+            modifier = Modifier
+                .background(color = Color(0xFFF8EEE2))
+                .fillMaxSize()
+        ) {
+            TopAppBar(
+                backgroundColor = Color(0xFFF8EEE2),
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        fontSize = TextUnit(32f, TextUnitType.Sp),
+                        fontWeight = FontWeight.Black,
+                        color = Color(0xFF403B36)
+                    )
+                },
+                actions = {
+                    IconButton(onClick = onAccountCenterClick) {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = "Account center",
+                            tint = Color(0xFF595550)
+                        )
+                    }
+                },
+                elevation = 0.dp
+            )
 
+            Spacer(modifier = Modifier.height(8.dp))
 
-
-//        val state by viewModel.state.collectAsStateWithLifecycle()
-//
-//        if (state.isEmpty) {
-//            EmptyNotes(
-//                onButtonClick = viewModel::toCreateNote
-//            )
-//        } else {
-//            Box(modifier = Modifier.fillMaxSize()) {
-//                Notes(
-//                    notes = state.notes,
-//                    onNoteClick = viewModel::toNote,
-//                    onNoteLongClick = viewModel::onDeleteNoteClick
-//                )
-//                Box(
-//                    modifier = Modifier.fillMaxSize(),
-//                    contentAlignment = Alignment.BottomEnd
-//                ) {
-//                    Button(
-//                        modifier = Modifier.padding(16.dp),
-//                        onClick =  viewModel::toCreateNote
-//                    ) {
-//                        Text(text = "Add note")
-//                    }
-//                }
-//            }
-//        }
+            if (state.isEmpty) {
+                EmptyNotes(onCreateNoteClick = onCreateNoteClick)
+            } else {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Notes(
+                        notes = state.notes,
+                        onOpenNoteClick = onOpenNoteClick,
+                        onDeleteNoteClick = onDeleteNoteClick
+                    )
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun EmptyNotes(onButtonClick: () -> Unit) {
+fun EmptyNotes(
+    onCreateNoteClick: () -> Unit
+) {
     Column(
         modifier = Modifier
+            .padding(
+                horizontal = 60.dp
+            )
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = onButtonClick) {
-            Text(text = "Tap to create new note")
+        Image(
+            modifier = Modifier
+                .size(200.dp),
+            painter = painterResource(id = R.drawable.start_your_jorney),
+            contentDescription = ""
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Start Your Journey",
+            fontSize = TextUnit(28f, TextUnitType.Sp),
+            fontWeight = FontWeight.ExtraBold,
+            color = Color(0xFF403B36),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Every big step start with small step. Notes your first idea and start your journey!",
+            fontSize = TextUnit(16f, TextUnitType.Sp),
+            fontWeight = FontWeight.Bold,
+            color = Color(0xAF595550),
+            textAlign = TextAlign.Center,
+            lineHeight = TextUnit(1.4f, TextUnitType.Em)
+        )
+
+        Image(
+            modifier = Modifier.size(128.dp),
+            painter = painterResource(id = R.drawable.arc_shape_arrow),
+            contentDescription = ""
+        )
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(54.dp),
+            shape = RoundedCornerShape(12.dp),
+            onClick = onCreateNoteClick,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFFD9614C)
+            ),
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp
+            )
+        ) {
+            Text(
+                text = "Create a Note",
+                lineHeight = TextUnit(1.4F, TextUnitType.Em),
+                fontSize = TextUnit(16F, TextUnitType.Sp),
+                color = Color.White
+            )
         }
     }
 }
@@ -145,19 +211,16 @@ fun EmptyNotes(onButtonClick: () -> Unit) {
 @Composable
 fun Notes(
     notes: List<Note>,
-    onNoteClick: (noteId: String) -> Unit,
-    onNoteLongClick: (noteId: String) -> Unit
+    onOpenNoteClick: (String) -> Unit,
+    onDeleteNoteClick: (String) -> Unit
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(items = notes, key = { it.id }) { note ->
             NoteCard(
                 title = note.title,
                 description = note.description,
-                onNoteClick = { onNoteClick(note.id) },
-                onNoteLongClick = { onNoteLongClick(note.id) }
+                onOpenNoteClick = { onOpenNoteClick(note.id) },
+                onDeleteNoteClick = { onDeleteNoteClick(note.id) }
             )
         }
     }
@@ -166,54 +229,65 @@ fun Notes(
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun NoteCard(
-    modifier: Modifier = Modifier,
     title: String,
     description: String,
-    onNoteClick: () -> Unit,
-    onNoteLongClick: () -> Unit
+    onOpenNoteClick: () -> Unit,
+    onDeleteNoteClick: () -> Unit
 ) {
     Surface(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(8.dp, 0.dp, 8.dp, 8.dp)
+            .padding(16.dp, 0.dp, 16.dp, 8.dp)
+            .border(
+                width = 0.dp,
+                color = Color(0xFFFFFDF0),
+                shape = RoundedCornerShape(12.dp)
+            )
             .combinedClickable(
-                onClick = onNoteClick,
-                onLongClick = onNoteLongClick
+                onClick = onOpenNoteClick,
+                onLongClick = onDeleteNoteClick
             ),
-        border = BorderStroke(
-            width = 1.dp,
-            color = Color.Gray
-        ),
-        shape = RoundedCornerShape(16.dp)
+        shape = RoundedCornerShape(12.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = Color(0xFFFFFDF0),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(12.dp)
+        ) {
             Text(
                 modifier = Modifier
                     .padding(bottom = 16.dp),
                 text = title,
-                fontSize = TextUnit(20f, TextUnitType.Sp),
-                fontWeight = FontWeight.Medium
+                fontSize = TextUnit(18f, TextUnitType.Sp),
+                fontWeight = FontWeight.ExtraBold,
+                color = Color(0xFF595550)
             )
-            Text(text = description)
+            Text(
+                text = description,
+                fontSize = TextUnit(16f, TextUnitType.Sp),
+                fontWeight = FontWeight.Medium,
+                lineHeight = TextUnit(1.3f, TextUnitType.Em),
+                color = Color(0xFF595550)
+            )
         }
     }
 }
 
 @Preview
 @Composable
-fun NotesPreview() {
-    NotesTheme {
-        Surface {
-            val list = listOf(
-                Note("Task", "Description"),
-                Note("Task1", "Description1"),
-                Note("Task3", "Description3")
-            )
-            EmptyNotes {
-
-            }
-            //Notes(list, {})
-        }
-    }
+fun NotesScreenPreview() {
+    NotesContent(
+        state = NotesState(
+            notes = listOf(Note("My first title", "To speak about my drawbacks you need to know how to deal with my assistants"))
+        ),
+        onCreateNoteClick = {},
+        onAccountCenterClick = {},
+        onOpenNoteClick = {},
+        onDeleteNoteClick = {}
+    )
 }
