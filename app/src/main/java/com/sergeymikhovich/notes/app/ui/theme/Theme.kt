db71bee94e8eeb4d9df74db818775bc3f9840aed
Sergey Mikhovich <srgmikh1998@gmail.com>
2024-09-25 15:44:53 +0300

    change light color scheme

--- app/src/main/java/com/sergeymikhovich/notes/app/ui/theme/Theme.kt
+++ app/src/main/java/com/sergeymikhovich/notes/app/ui/theme/Theme.kt
@@ -23,7 +23,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = /*Purple40*/ Color(0xFFF8EEE2),
+    primary = Purple40 /*Color(0xFFF8EEE2)*/,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
